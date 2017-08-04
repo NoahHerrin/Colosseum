ddba8e979ceb20390cfc7f19306c216d9e8118ae
NoahHerrin <noahherrin@gmail.com>
2017-08-03 19:19:39 -0700

    Front-end account system

--- public/authentication/test.php
+++ public/authentication/test.php
@@ -7,27 +7,27 @@
 function main() {
     debug("hello world");
 
-    $address = "localhost";
-    $username = "root";
-    $password = "root";
+    $address = "198.71.225.59";
+    $username = "Noah";
+    $password = "1x8sf^H0";
 
-// Create connection
+    // Create connection
     $conn = new mysqli($address, $username, $password);
 
-// Check connection
+    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
     echo "Connected successfully";
-    $sql = "USE ACCOUNT;";
+    $sql = "USE AllTheData;";
     $conn->query($sql);
     signup($conn,"noahherrin@gmail.com","havocnh","secretword");
-    login($conn,"noahherrin@gmail.com","secretword");
+    login($conn,"noahherrin@gmail.com","notthesecretword");
     $conn->close();
 }
 function login($conn,$email,$password) {
 //get unique id and compare passwords
-    $sql = "SELECT password FROM Accounts WHERE email='$email' LIMIT 1";
+    $sql = "SELECT password FROM Account WHERE email='$email' LIMIT 1";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
 
@@ -43,16 +43,15 @@ function login($conn,$email,$password) {
 
 function signup($connection,$email, $uuid, $pwd) {
     debug("Attempting Signup");
-    $input_validated = true;
-    if($input_validated) {
-        $stmt = $connection->prepare("INSERT INTO Accounts (email, username, password) VALUES (?, ?, ?)");
+
+        $stmt = $connection->prepare("INSERT INTO Account (email, username, password) VALUES (?, ?, ?)");
         $options = [ 'cost'=>9,];
         $hash = password_hash($pwd,PASSWORD_BCRYPT,$options);
         $stmt->bind_param("sss", $email, $uuid, $hash);
         $stmt->execute();
         $stmt->close();
         debug("Signup Complete!");
-    }
+
 
 }
 
